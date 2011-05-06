5600e294d12b0c0052eb0563bc49098ac205d202
bap2000 <bap-git@a1.org.uk>
2011-05-06 12:03:44 +0100

    [FIXED JENKINS-9480] - request for textarea instead of textbox for exec command

---
+++ src/main/java/jenkins/plugins/publish_over/JellySupport.java
@@ -0,0 +1,41 @@
+/*
+ * The MIT License
+ *
+ * Copyright (C) 2010-2011 by Anthony Robinson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jenkins.plugins.publish_over;
+
+import java.util.regex.Pattern;
+
+public class JellySupport {
+
+    public static final int MINIMUM_MINIMUM_HEIGHT = 1;
+    public static final int DEFAULT_MINIMUM_HEIGHT = 5;
+    private static final Pattern LINE_END = Pattern.compile("\r?\n");
+
+    public static final int textAreaHeight(final int minimum, final String content) {
+        final int min = Math.max(minimum, MINIMUM_MINIMUM_HEIGHT);
+        if (content == null) return min;
+        return Math.max(min, LINE_END.split(content).length);
+    }
+
+}
---
+++ src/test/java/jenkins/plugins/publish_over/JellySupportTest.java
@@ -0,0 +1,60 @@
+/*
+ * The MIT License
+ *
+ * Copyright (C) 2010-2011 by Anthony Robinson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jenkins.plugins.publish_over;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class JellySupportTest {
+
+    @Test public void textAreaHeightMinimumIfNoContent() {
+        assertEquals(3, JellySupport.textAreaHeight(3, null));
+    }
+
+    @Test public void textAreaHeightMinimumIfEmpty() {
+        assertEquals(1, JellySupport.textAreaHeight(1, ""));
+    }
+
+    @Test public void textAreaHeightUnixContentLines() {
+        assertEquals(3, JellySupport.textAreaHeight(1, "one\ntwo\nthree"));
+    }
+
+    @Test public void textAreaHeightWindowsContentLines() {
+        assertEquals(3, JellySupport.textAreaHeight(1, "one\r\ntwo\r\nthree"));
+    }
+
+    @Test public void textAreaHeightMinimumIfGreaterThanHeight() {
+        assertEquals(3, JellySupport.textAreaHeight(3, "one\ntwo"));
+    }
+
+    @Test public void textAreaHeightContentHeightIfGreaterThanMinimum() {
+        assertEquals(3, JellySupport.textAreaHeight(2, "one\ntwo\nthree"));
+    }
+
+    @Test public void textAreaHeightEnforceMinimumMinimum() {
+        assertEquals(JellySupport.MINIMUM_MINIMUM_HEIGHT, JellySupport.textAreaHeight(JellySupport.MINIMUM_MINIMUM_HEIGHT -1, null));
+    }
+
+}
